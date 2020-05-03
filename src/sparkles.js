58f9bda4fe91b858eb189a417dbd4faaadb41f12
Mark Foster <mark.foster@abracontrols.com>
2020-05-03 11:01:59 -0600

    add color

--- src/sparkles.js
+++ src/sparkles.js
@@ -11,6 +11,7 @@ var app = new Vue({
   mounted() {
     this.canvas = document.getElementById('sparkles');
     this.ctx = this.canvas.getContext('2d');
+    this.ctx.globalCompositeOperation = "xor";
     this.updateSize();
     window.addEventListener('resize', this.updateSize);
     this.fill('rgba(0,0,0,1)');
@@ -22,7 +23,7 @@ var app = new Vue({
 
   methods: {
     createSparks() {
-      let size = 500;
+      let size = 100;
       while(size--) {
         this.sparks.push(this.createSpark()); 
       }
@@ -32,12 +33,12 @@ var app = new Vue({
       return {
         pos: {
           x: this.canvas.width / 2,
-          y: this.canvas.height - 50,
+          y: this.canvas.height - this.canvas.height / 3,
         },
-        life: 500,
-        lifeMax: 500,
+        life: 800,
+        lifeMax: 800,
         angle: 25 + Math.random() * 130,
-        speed: 1 + Math.random() * 10,
+        speed: 2 + Math.random() * 5,
       };
     },
 
@@ -45,24 +46,29 @@ var app = new Vue({
       this.fill('rgba(0,0,0,0.1)');
       this.sparks.forEach((spark,i) => {
         let thisRand = Math.random();
+        let colorRand = Math.random();
         let op = 1 / spark.lifeMax * spark.life;
-        this.ctx.fillStyle =  thisRand < 0.2 ? `rgba(255,0,0,${op})` : thisRand < 0.4 ?  `rgba(255,255,0,${op})`: thisRand < 0.6 ?  `rgba(255,125,0,${op})` : `rgba(255,255,255,${op})`; 
-        let sz = 3 * spark.life / spark.lifeMax;
-        this.ctx.beginPath();
-        this.drawSphere(this.ctx, {x:spark.pos.x, y:spark.pos.y}, sz * thisRand , true, false);
-        this.ctx.lineWidth = thisRand * 0.2;
-        this.ctx.strokeStyle = thisRand < 0.2 ? `rgba(255,0,0,${op})` : thisRand < 0.4 ?  `rgba(255,255,0,${op})`: thisRand < 0.6 ?  `rgba(255,125,0,${op})` : `rgba(255,255,255,${op})`; 
+        let sz = 2 * spark.life / spark.lifeMax;
         let spike = 20 * (spark.life / spark.lifeMax) * thisRand;
         let spikeAngle = thisRand * 360;
         let spikeSin = Math.sin(spikeAngle * Math.PI / 180) * spike;
         let spikeCos = Math.cos(spikeAngle * Math.PI / 180) * spike;
+        let sin = Math.sin(spark.angle * Math.PI / 180) * spark.speed;
+        let cos = Math.cos(spark.angle * Math.PI / 180) * spark.speed;
+
+        this.ctx.fillStyle = this.ctx.strokeStyle =
+          colorRand < 0.2 ? `rgba(255,0,0,${op})` :
+          colorRand < 0.4 ?  `rgba(0,255,0,${op})`:
+          colorRand < 0.6 ?  `rgba(0,0,255,${op})` :
+          this.randomColor(op);
+        this.ctx.beginPath();
+        this.ctx.lineWidth = thisRand * 0.3;
         this.lineTo(this.ctx, spark.pos.x - spikeSin, spark.pos.y - spikeCos, spark.pos.x + spikeSin, spark.pos.y + spikeCos,);
         this.lineTo(this.ctx, spark.pos.x - spikeSin, spark.pos.y + spikeCos, spark.pos.x + spikeSin, spark.pos.y - spikeCos,);
         spike = 20 * (spark.life / spark.lifeMax) * Math.random();// reset
         this.lineTo(this.ctx, spark.pos.x - spikeCos, spark.pos.y - spikeSin, spark.pos.x + spikeCos, spark.pos.y + spikeSin,);
         this.lineTo(this.ctx, spark.pos.x - spikeCos, spark.pos.y + spikeSin, spark.pos.x + spikeCos, spark.pos.y - spikeSin,);
-        let sin = Math.sin(spark.angle * Math.PI / 180) * sz * spark.speed;
-        let cos = Math.cos(spark.angle * Math.PI / 180) * sz * spark.speed;
+        this.drawSphere(this.ctx, {x:spark.pos.x, y:spark.pos.y}, sz * thisRand , true, false);
         spark.pos.x += cos;
         spark.pos.y -= sin - (spark.lifeMax / spark.life) * 2;
         spark.life -= 1 * spark.speed;
@@ -96,11 +102,19 @@ var app = new Vue({
       ctx.lineTo(x2, y2)
       ctx.stroke()
     },
+    randomColor(op = 0.5) {
+      const clr = `rgba(
+        ${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)},
+        ${Math.round(Math.random() * 255)}, ${op})`;
+      console.log(clr);
+      return clr;
+    },
   },
-
   beforeDestroy() {
     if (this.timer) {
       clearInterval(this.timer);
     }
   }
+
 });
